889028f42b1234ae86a7023c75f1c0fc26e126b8
Philip Håkans <philip.hakans@gmail.com>
2016-01-27 11:10:19 +0100

    Update pageScript.js
    Added additional code.

--- pageScript.js
+++ pageScript.js
@@ -1,4 +1,20 @@
-var button = document.getElementByID('menuItemGroup')
+var button = document.getElementById('menuItemGroup');
+var link = document.getElementById('menuItemGroup').getElementsByTagName('a')[0].innerHTML;
+var contentBox = document.getElementById('contentBox');
+
 
 button.style.cursor = 'pointer';
-button.onClick = 
+button.onClick = clickButton();
+
+
+function clickButton() {
+
+	if(link = "Tools") {
+		document.getElementById('contentBox')
+		contentBox.innerHTML("
+				<p>Intranet</p>
+				<p>RDP</p>
+			")
+	}
+
+}
